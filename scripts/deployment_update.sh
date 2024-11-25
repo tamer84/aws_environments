b70211e768ed9cd9afdb251d958876802406aee6
Tamer Husnu <tamer.husnu@crealogix.com>
2024-11-25 14:20:13 +0100

    Clean history for public

--- scripts/deployment_update.sh
+++ scripts/deployment_update.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-
-# This script updates a new or existing environment (runs terraform apply)
-# Per default it runs terraform apply with --auto-approve
-# If you want to run 'plan' pass 'plan' as a 5th parameter
-
 set -e
 
 echo "Starting script with parameters" "$@"
 
-. ./deployment_functions.sh
-
-export BUSINESSUNIT=${1}
-export SOLUTION=${2}
-export ENVIRONMENT=${3}
-export ACCOUNT=${4}
+export SOLUTION=${1}
+export ENVIRONMENT=${2}
+export ACCOUNT=${3}
 export TERRAFORM_COMMAND=${5:-apply}
 
-validate_args
-validate_aws_deployment_account
-
-CONFIG_ROOT="../deployments/${BUSINESSUNIT}/${SOLUTION}/${ENVIRONMENT}"
+CONFIG_ROOT="../deployments/${SOLUTION}/${ENVIRONMENT}"
 CONFIG_PATH="${CONFIG_ROOT}/configuration"
 BACKEND_CONFIG_FILE="${CONFIG_ROOT}/backend/backend.hcl"
 if [ "${TERRAFORM_COMMAND}" == "apply" ]; then
