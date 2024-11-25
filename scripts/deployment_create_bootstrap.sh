b35eebe29fbb5af63869c9bab061d37a0539e135
Tamer Husnu <tamer.husnu@crealogix.com>
2024-11-25 14:15:52 +0100

    Clean history for public

--- scripts/deployment_create_bootstrap.sh
+++ scripts/deployment_create_bootstrap.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-
-# This script only runs the bootstrap step (setup s3 bucket and dynamo-db table)
-
 set -e
 
 echo "Starting script with parameters" "$@"
 
-. ./deployment_functions.sh
+export SOLUTION=${1}
+export ENVIRONMENT=${2}
+export ACCOUNT=${3}
 
-export BUSINESSUNIT=${1}
-export SOLUTION=${2}
-export ENVIRONMENT=${3}
-export ACCOUNT=${4}
-
-validate_args
-validate_aws_deployment_account
-
-CONFIG_ROOT="../deployments/${BUSINESSUNIT}/${SOLUTION}/${ENVIRONMENT}"
+CONFIG_ROOT="../deployments/${SOLUTION}/${ENVIRONMENT}"
 CONFIG_PATH="${CONFIG_ROOT}/configuration"
 TERRAFORM_OPTIONS="--auto-approve"
 
