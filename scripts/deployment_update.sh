d14a74238836a9e03b013aa8e5860013890520c9
Tamer Husnu <84tamer@gmail.com>
2024-12-06 10:13:58 +0100

    Release for public

---
+++ scripts/deployment_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Starting script with parameters" "$@"
+
+export SOLUTION=${1}
+export ENVIRONMENT=${2}
+export ACCOUNT=${3}
+
+CONFIG_ROOT="../deployments/${SOLUTION}/${ENVIRONMENT}"
+CONFIG_PATH="${CONFIG_ROOT}/configuration"
+BACKEND_CONFIG_FILE="${CONFIG_ROOT}/backend/backend.hcl"
+
+cd ../infra_resources
+terraform init -backend-config=${BACKEND_CONFIG_FILE} -reconfigure || terraform init -backend-config=${BACKEND_CONFIG_FILE}
+terraform workspace select -or-create ${ENVIRONMENT}
+terraform apply -var-file ${CONFIG_PATH}/infra_resources.tfvars --auto-approve
